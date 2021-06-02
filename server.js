3c717238a7e09bbc005b115c0ee457c8a72c6659
caitlindavey <caitlin@caitlindavey.com>
2021-06-02 19:44:31 -0400

    added sounds

--- server.js
+++ server.js
@@ -50,7 +50,8 @@ app.post('/completed', (request, response) => {
 
 	twiml.redirect('/completed');
 
-	sendResponse(response, twiml);
+	response.type('text/xml');
+	response.send(twiml.toString());
 });
 
 app.post('/deliver', (request, response) => {
@@ -59,7 +60,7 @@ app.post('/deliver', (request, response) => {
 	accomplishment = request.body.SpeechResult;
 
 	//Parse speech result to remove "I"
-	//twiml.play('audio/celebration.mp3');
+	twiml.play('https://files.mattbutterfield.com/celebration.mp3');
 	twiml.say({voice: 'Polly.Amy'}, `${callerName}. You did it! It is an honor to address you today because despite all the odds you persevered and you ${accomplishment}. In the words of Les Brown, “Shoot for the moon. Even if you miss, you’ll land amongst the stars.” Whether you’re basking in the moon’s glow or swimming amongst the stars, know that you did it. I hope  you take time to celebrate yourself today for boldly charting your path forward to success.`);
 	
 	//Replay or hang-up
@@ -69,7 +70,8 @@ app.post('/deliver', (request, response) => {
 	});
 	gather.say({voice: 'Polly.Amy'},`To hear your speech again, press 1. To hangup, press 2.`);
 
-	sendResponse(response, twiml);
+	response.type('text/xml');
+	response.send(twiml.toString());
 });
 
 app.post('/gather', (request, response) => {
@@ -91,7 +93,8 @@ app.post('/gather', (request, response) => {
 		twiml.say({voice: 'Polly.Amy'},`Thanks so much for stopping by, I think this is farewell!`);
    		twiml.hangup();
 	}
-	sendResponse(response, twiml);
+	response.type('text/xml');
+	response.send(twiml.toString());
 });
 
 //render the response as XML in reply to the webhook request -- is this the right place for this? 
