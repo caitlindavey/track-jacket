f5c15435fdd88a7eeec1cf64b39e238f0e8c000e
caitlindavey <caitlin@caitlindavey.com>
2021-06-02 20:06:11 -0400

    fixed bug

--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 const express = require('express');
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const urlencoded = require('body-parser').urlencoded; 
-let accomplishment; 
-let callerName; 
 
 //Server
 const app = express();
@@ -39,7 +37,7 @@ app.post('/voice', (request, response) => {
 app.post('/completed', (request, response) => {
 
 	const twiml = new VoiceResponse();
-	callerName = request.body.SpeechResult;
+	const callerName = request.body.SpeechResult;
 
 	const gatherNode = twiml.gather({ 
 		input: 'speech',
@@ -50,28 +48,26 @@ app.post('/completed', (request, response) => {
 
 	twiml.redirect('/completed');
 
-	response.type('text/xml');
-	response.send(twiml.toString());
+	sendResponse(response, twiml);
 });
 
-app.post('/deliver', (request, response) => {
+app.post('/deliver', (request, response, callerName) => {
 
 	const twiml = new VoiceResponse();
-	accomplishment = request.body.SpeechResult;
+	const accomplishment = request.body.SpeechResult;
 
-	//Parse speech result to remove "I"
 	twiml.play('https://files.mattbutterfield.com/celebration.mp3');
-	twiml.say({voice: 'Polly.Amy'}, `${callerName}. You did it! It is an honor to address you today because despite all the odds you persevered and you ${accomplishment}. In the words of Les Brown, “Shoot for the moon. Even if you miss, you’ll land amongst the stars.” Whether you’re basking in the moon’s glow or swimming amongst the stars, know that you did it. I hope  you take time to celebrate yourself today for boldly charting your path forward to success.`);
+
+	twiml.say({voice: 'Polly.Amy'}, `You did it! It is an honor to address you today because despite all the odds you persevered and you ${accomplishment}. In the words of Les Brown, “Shoot for the moon. Even if you miss, you’ll land amongst the stars.” Whether you’re basking in the moon’s glow or swimming amongst the stars, know that you did it. I hope  you take time to celebrate yourself today for boldly charting your path forward to success.`);
 	
 	//Replay or hang-up
 	const gather = twiml.gather({
 		numDigits: 1,
 		action: '/gather',
 	});
-	gather.say({voice: 'Polly.Amy'},`To hear your speech again, press 1. To hangup, press 2.`);
+	gather.say({voice: 'Polly.Amy'},`To revel in your accomplishment once more press 1. To hangup, press 2.`);
 
-	response.type('text/xml');
-	response.send(twiml.toString());
+	sendResponse(response, twiml);
 });
 
 app.post('/gather', (request, response) => {
@@ -80,8 +76,7 @@ app.post('/gather', (request, response) => {
 	if (request.body.Digits) {
 		switch (request.body.Digits) {
 			case '1': 
-				accomplishment = request.body.SpeechResult;
-				twiml.say({voice: 'Polly.Amy'}, `Once more just for you, ${callerName}. You did it! It is an honor to address you today because despite all the odds you persevered and you ${accomplishment}. In the words of Les Brown, “Shoot for the moon. Even if you miss, you’ll land amongst the stars.” Whether you’re basking in the moon’s glow or swimming amongst the stars, know that you did it. I hope  you take time to celebrate yourself today for boldly charting your path forward to success. Bye now!`);
+				twiml.say({voice: 'Polly.Amy'}, `Ok, one more time! You did it! Woooooo!`);
 	        	break;
 		    case '2':
 		   		twiml.say({voice: 'Polly.Amy'},`Goodbye!`);
@@ -93,8 +88,7 @@ app.post('/gather', (request, response) => {
 		twiml.say({voice: 'Polly.Amy'},`Thanks so much for stopping by, I think this is farewell!`);
    		twiml.hangup();
 	}
-	response.type('text/xml');
-	response.send(twiml.toString());
+	sendResponse(response, twiml);
 });
 
 //render the response as XML in reply to the webhook request -- is this the right place for this? 
